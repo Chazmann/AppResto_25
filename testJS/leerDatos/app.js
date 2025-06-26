4733db5147cdc72dee80600c460867aeb29c8939
chazmann <lchaz85@gmail.com>
2025-06-26 20:53:33 -0300

    Añadir imágenes de restaurantes y mejorar la estructura de la API de Restaurantes y Platos

---
+++ testJS/leerDatos/app.js
@@ -0,0 +1,33 @@
+const archivoJson = 'datos.json'; 
+// ruteo al archivo JSON que contiene los datos
+async function obtenerDatos() {
+    try {
+        // uso fetch para obtener los datos del archivo JSON
+        const response = await fetch(archivoJson);
+        // manejo de errores si la respuesta no es correcta
+        if (!response.ok) {
+            throw new Error(`Error al cargar los datos: ${response.statusText}`);
+        }
+// convierto la respuesta a formato JSON
+       
+        const datos = await response.json();
+ // y luego itero sobre los datos para mostrarlos en la lista
+ 
+        const listaDatos = document.getElementById('lista-datos');
+        datos.forEach(usuario => {
+            const li = document.createElement('li');
+            li.textContent = `ID: ${usuario.id}, Nombre: ${usuario.nombre}, Edad: ${usuario.edad}, Ciudad: ${usuario.ciudad}`;
+            listaDatos.appendChild(li);
+        });
+// manejo de errores en caso de que ocurra algún problema durante la operación fetch
+    } catch (error) {
+
+        console.error('Hubo un problema con la operación fetch:', error);
+        // muestro un mensaje de error en la lista
+        const listaDatos = document.getElementById('lista-datos');
+        listaDatos.textContent = 'No se pudieron cargar los datos.';
+    }
+}
+// evento que se dispara cuando el DOM está completamente cargado
+// para asegurarme de que el script se ejecute después de que el contenido esté listo
+document.addEventListener('DOMContentLoaded', obtenerDatos);
